e411196e8e68ab77385452896a84c4e7b1832498
Dmitry Lyusko <lyusko.dm@gmail.com>
2023-01-24 19:15:33 +0300

    feat(api): account creation

--- src/services/solana/account-service.js
+++ src/services/solana/account-service.js
@@ -1,6 +1,17 @@
-import { PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js'
+import {
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  Keypair,
+  Transaction,
+  SystemProgram,
+} from '@solana/web3.js'
+import { NATIVE_MINT, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { LoggerTool } from '../../tools/logger-tool.js'
 import { SolanaTestAccountService } from './test-account-service.js'
+import { base58_to_binary, binary_to_base58 } from 'base58-js'
+import { ACCOUNT_SIZE } from '@solana/spl-token'
+import { getMinimumBalanceForRentExemptAccount } from '@solana/spl-token'
+import { createInitializeAccountInstruction } from '@solana/spl-token'
 
 export class SolanaAccountService {
   #connection
@@ -9,6 +20,69 @@ export class SolanaAccountService {
   constructor(connection, network) {
     this.#network = network
     this.#connection = connection
+  }
+
+  async createRandomAccount(
+    feePayerPrivateKey,
+    log_opts = { logCreatedAccount: false }
+  ) {
+    let feePayerKeyPair = Keypair.fromSecretKey(
+      base58_to_binary(feePayerPrivateKey)
+    )
+    let newAccountKeyPair = Keypair.generate()
+
+    LoggerTool.silly(
+      this.#serviceName,
+      `(${this.#network})[READY] Create random account`,
+      log_opts.logCreatedAccount
+        ? [
+            [
+              'New data',
+              newAccountKeyPair.publicKey.toBase58(),
+              binary_to_base58(newAccountKeyPair.secretKey),
+            ],
+            [
+              'Fee payer data',
+              feePayerKeyPair.publicKey.toBase58(),
+              feePayerPrivateKey,
+            ],
+          ]
+        : null
+    )
+    const transaction = new Transaction()
+    const lamports = await getMinimumBalanceForRentExemptAccount(
+      this.#connection
+    )
+    transaction.add(
+      SystemProgram.createAccount({
+        fromPubkey: feePayerKeyPair.publicKey,
+        newAccountPubkey: newAccountKeyPair.publicKey,
+        space: ACCOUNT_SIZE,
+        lamports: lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      SystemProgram.transfer({
+        fromPubkey: feePayerKeyPair.publicKey,
+        toPubkey: newAccountKeyPair.publicKey,
+        lamports: lamports,
+      })
+    )
+
+    const response = await this.#connection.sendTransaction(transaction, [
+      feePayerKeyPair,
+      newAccountKeyPair,
+    ])
+
+    LoggerTool.silly(
+      this.#serviceName,
+      `(${this.#network})[SUCCESS] Created random account`,
+      log_opts.logCreatedAccount
+        ? [
+            newAccountKeyPair.publicKey.toBase58(),
+            binary_to_base58(newAccountKeyPair.secretKey),
+          ]
+        : null
+    )
   }
 
   /**
